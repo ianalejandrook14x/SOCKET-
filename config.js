4fbc2271ddd0e7bfd92849fb5685ad97954ad23a
Iᴀɴ.xʏᴢ <taiwam34@gmail.com>
2026-08-22 01:16:04 -0300

    JS

--- config.js
+++ config.js
@@ -1,37 +1,19 @@
 /*
-•❅──────✧✦✧──────❅•
-Codigo Creado Por CUERVO-TEAM-SUPREME
-Para Elymas-Bot Este Codigo Es 
-Exclusivo Y Unico Para Este Bot Al 
-Clonar O Copiar Dejar Estos Creditos 
-De Cuervo-Team-Supreme
-━━━━━ ☾☽ ━━━━━
-ʚĭɞ ೃ CODIGO JAVASCRIPT ʚĭɞ ೃ
-ʚĭɞ ೃ codigo :: config.js
-ʚĭɞ ೃ funcion :: configuracion principal del bot
-ʚĭɞ ೃ estado :: completo
-──────✧✦✧──────
+Fixxedd
 */
 
 export const config = {
 
-    botName: 'Cuervo',
-    ownerName: 'TheDevil',
+    botName: 'F I X X E D',
+    ownerName: 'TewIanIx',
 
     owners: [
-        // The Devil (numero off)
-        '886958381686',
-        '250405971824657',
-        // The Devil (numero nuevo)
-        '818021404021',
-        '47344095637596'
+        // Ian
+        '5493876639332'
     ],
 
     prefixes: [
-        '.',
-        '#',
-        '!',
-        '/'
+        '#'
     ],
 
     sessionName: './sessions/principal',
@@ -44,9 +26,9 @@ export const config = {
     loggerLevel: 'silent',
 
     menu: {
-        title: '🌱 CUERVO BOT',
-        description: 'Bot de WhatsApp modular',
-        footer: '© Cuervo Bot'
+        title: 'F I X X E D',
+        description: 'WhatsApp bot',
+        footer: 'By TewIanIx'
     },
 
     subbots: {
