f99a8ccb39eb6f67452ce56859a8d8a7e432a3d0
Iᴀɴ.xʏᴢ <taiwam34@gmail.com>
2026-09-10 23:25:56 -0300

    -

---
+++ plugins/Jadibot/viewJadi.js
@@ -0,0 +1,36 @@
+import { getAllSubBots } from '../../lib/subbots.js'
+
+export default {
+    command: ['bots', 'jadibots', 'sockets'],
+
+    async run(m) {
+        const subbotsObject = getAllSubBots() || {}
+        
+        // (Array)
+        const botsList = Object.values(subbotsObject)
+
+        if (botsList.length === 0) {
+            return m.reply('*No se encontrarón Jadibots.*')
+        }
+
+        let texto = `     *ᴄᴏɴɴᴇᴄᴛᴇᴅ ᴊᴀᴅɪʙᴏᴛꜱ*     \n│\n`
+
+        let contador = 1
+
+        for (const bot of botsList) {
+            const isConnected = bot.connected || bot.status === 'connected'
+            const status = isConnected ? 'ᴄᴏɴᴇᴄᴛᴀᴅᴏ' : 'ᴅᴇꜱᴄᴏɴᴇᴄᴛᴀᴅᴏ'
+            
+            // Limpiar el JID para mostrar solo el número limpio
+            const cleanJid = bot.jid ? bot.jid.split('@')[0] : 'Desconocido'
+
+            texto += `${contador} | ${cleanJid} | ${status}`
+
+            contador++
+        }
+
+        texto += `\n____________`
+
+        await m.reply(texto)
+    }
+}
